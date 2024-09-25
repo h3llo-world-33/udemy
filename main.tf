82c24f47ca0b1a581f4f30cc2b98b02d67a5e588
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-09-25 15:50:05 +0100

    configuring target groups and health checks

--- main.tf
+++ main.tf
@@ -75,8 +75,23 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
+# get the details of default vpc
+data "aws_vpc" "default" {
+  default = true
+}
+
+# use the default vpc id and get the default subnets ids 
+data "aws_subnet_ids" "default_subnet_ids" {
+  vpc_id = data.aws_vpc_default.default.id
+}
+
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  # use the default subnet ids
+  vpc_zone_identifier = data.aws_subnet_ids.default_subnet_ids.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -87,3 +102,63 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default_subnet_ids.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response = {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-example-target-group"
+
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
