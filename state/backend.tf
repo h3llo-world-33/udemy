a9485d84e00ec2452312933c2ff602ca3b42815a
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-09-26 11:16:57 +0100

    debugging line 42

--- state/backend.tf
+++ state/backend.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = terraform-masterclass-locks-table
+  name = "terraform-masterclass-locks-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
 
